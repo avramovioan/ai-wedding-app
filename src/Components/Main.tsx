d87971cf6f03b7a0e5f8ffb937a128a84451b81a
Ioan Avramov <avramovioan@gmail.com>
2023-04-24 19:19:05 +0300

    fixed overflow bug

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -4,22 +4,28 @@ import Timer from "./Timer";
 
 function Main() {
   return (
-    <div className="flex h-full flex-col overflow-auto ">
-      <InvitationLayout />
-      <div className="relative h-full bg-gradient-to-tr from-[rgb(72,116,101)] to-[#1d2e28] ">
-        <div className="mt-4">
-          <fieldset className="border-t border-slate-200">
-            <legend className="mx-auto italic text-white">
-              <Timer weddingDate={new Date(2023, 7, 25, 17, 30)} />
-            </legend>
-          </fieldset>
-        </div>
+    <>
+      <div className="fixed inset-0 -z-10 bg-gradient-to-tr from-[rgb(72,116,101)] to-[#1d2e28] " />
+      <div
+        style={{ overflow: "overlay" }}
+        className="flex flex-col overflow-y-scroll"
+      >
+        <InvitationLayout />
+        <div className="flex-1 ">
+          <div className="mt-4">
+            <fieldset className="border-t border-slate-200">
+              <legend className="mx-auto italic text-white">
+                <Timer weddingDate={new Date(2023, 7, 25, 17, 30)} />
+              </legend>
+            </fieldset>
+          </div>
 
-        <div className="flex justify-center">
-          <Tabs />
+          <div className="flex justify-center">
+            <Tabs />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/Components/Tabs.tsx
+++ src/Components/Tabs.tsx
@@ -12,11 +12,7 @@ function classNames(...classes: any[]) {
 }
 
 type POST = {
-  id: number;
-  title: string;
-  date: string;
-  commentCount: number;
-  shareCount: number;
+  texts?: string[];
 };
 
 type TabObj = {
@@ -32,18 +28,52 @@ const tabs: TabObj[] = [
     icon: schedule,
     posts: [
       {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5,
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
       },
       {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2,
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
+      },
+      {
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
+      },
+      {
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
+      },
+      {
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
+      },
+      {
+        texts: [
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+          "АААААААААААААААА",
+        ],
       },
     ],
     index: 0,
@@ -51,22 +81,7 @@ const tabs: TabObj[] = [
   {
     name: "test2",
     icon: rings,
-    posts: [
-      // {
-      //   id: 1,
-      //   title: 'Ask Me Anything: 10 answers to your questions about coffee',
-      //   date: '2d ago',
-      //   commentCount: 9,
-      //   shareCount: 5,
-      // },
-      // {
-      //   id: 2,
-      //   title: "The worst advice we've ever heard about coffee",
-      //   date: '4d ago',
-      //   commentCount: 1,
-      //   shareCount: 2,
-      // },
-    ],
+    posts: [],
     index: 1,
   },
   {
@@ -94,7 +109,7 @@ const tabs: TabObj[] = [
 export default function Tabs() {
   const [selectedIndex, setSelectedIndex] = useState(1);
   return (
-    <div className="w-full max-w-5xl py-3 sm:px-0 md:mt-4">
+    <div className="h-full max-w-5xl py-3 sm:px-0 md:mt-4">
       <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
         <Tab.List className="flex justify-center">
           {tabs.map((tab) => (
@@ -121,35 +136,18 @@ export default function Tabs() {
             </Tab>
           ))}
         </Tab.List>
-        <Tab.Panels className="mt-2">
+        <Tab.Panels className="mt-4">
           {tabs.map((tab, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                "rounded-xl p-3",
-                "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none"
-              )}
-            >
-              <ul>
-                {tab.posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
-                  >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-                  </li>
-                ))}
-              </ul>
+            <Tab.Panel key={idx} className={classNames("focus:outline-none")}>
+              {tab.posts.map((post) => (
+                <div className="mt-5 w-full flex-col">
+                  {post.texts?.map((text) => (
+                    <div className="flex w-full justify-center text-xl text-white md:text-2xl">
+                      <p className="font-serif">{text}</p>
+                    </div>
+                  ))}
+                </div>
+              ))}
             </Tab.Panel>
           ))}
         </Tab.Panels>
