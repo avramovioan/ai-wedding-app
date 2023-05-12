f5ee5d8004ae6a883c71711238707d84c882bf2e
Ioan Avramov <avramovioan@gmail.com>
2023-05-12 23:01:45 +0300

    recoupled the object

--- src/Components/Tabs.tsx
+++ src/Components/Tabs.tsx
@@ -15,144 +15,141 @@ type POST = {
   texts?: string[];
 };
 
-type TabObj = {
-  name: string;
-  icon: string;
-  posts: POST[];
-  index: number;
-};
-
-const tabs: TabObj[] = [
+const invitation: { text: string[] }[] = [
   {
-    name: "test",
-    icon: schedule,
-    posts: [],
-    index: 0,
+    text: [
+      "В парка в онзи слънчев ден",
+      "се срещтна ти с мен.",
+      "И всичко почна като на игра,",
+      "но накрая някой изгоря!",
+    ],
   },
   {
-    name: "test2",
-    icon: rings,
-    posts: [
-      {
-        texts: [
-          "В парка в онзи слънчев ден",
-          "се срещтна ти с мен.",
-          "И всичко почна като на игра,",
-          "Но накрая някой изгоря!",
-        ],
-      },
-      {
-        texts: [
-          "Ето че се влюби ти в мен,",
-          "а аз отдавна вече бях в плен",
-          "и така започна нашата история",
-          "изпълнена с любов и еуфория! ",
-        ],
-      },
-      {
-        texts: [
-          "След вече две години се реши,",
-          "пък и обичаме се до уши,",
-          "та сватба ще направим",
-          "и хич? няма да се бавим!",
-        ],
-      },
-      {
-        texts: [
-          "Тъй като на Валя е вината",
-          "сега тя ще е кумата!",
-          "A Весо с пръстена помогна",
-          "и кумстване го погна!",
-        ],
-      },
-      {
-        texts: [
-          "На 25.08.2023 ще е тържеството,",
-          "за това си стягай облеклото",
-          "и горе в дясно потвърди,",
-          "защото поканен си и ти!",
-        ],
-      },
-      {
-        texts: [
-          "И недей да мислиш за подарък!",
-          "Просто постави го в плика малък",
-          "и на сватбата ни ти ела,",
-          "че да падне чудна веселба!",
-        ],
-      },
+    text: [
+      "Ето че се влюби ти в мен,",
+      "а аз отдавна вече бях в плен.",
+      "Така започна нашата история",
+      "изпълнена с любов и еуфория! ",
     ],
-    index: 1,
   },
   {
-    name: "test3",
-    icon: wedding_location,
-    posts: [
-      // {
-      //   id: 1,
-      //   title: "Ask Me Anything: 10 answers to your questions about coffee",
-      //   date: "2d ago",
-      //   commentCount: 9,
-      //   shareCount: 5,
-      // },
-      // {
-      //   id: 2,
-      //   title: "The worst advice we've ever heard about coffee",
-      //   date: "4d ago",
-      //   commentCount: 1,
-      //   shareCount: 2,
-      // },
+    text: [
+      "След две години вече се реши,",
+      "пък и влюбени сме до уши,",
+      "та сватба ще направим",
+      "и хич няма да се бавим!",
     ],
-    index: 2,
+  },
+  {
+    text: [
+      "Тъй като на Валя е вината",
+      "сега тя ще е кумата!",
+      "A Весо с пръстена помогна",
+      "и кумстване го погна!",
+    ],
+  },
+  {
+    text: [
+      "На 25.08.2023 (петък) ще е тържеството,",
+      "затова си стягай облеклото!",
+      "И горе в дясно потвърди,",
+      "защото поканен си и ти!",
+    ],
+  },
+  {
+    text: [
+      "Но недей да мислиш за подарък,",
+      "просто постави го в плика малък!",
+      "И на сватбата ни ти ела,",
+      "че да падне чудна веселба!",
+    ],
   },
 ];
+
 export default function Tabs() {
   const [selectedIndex, setSelectedIndex] = useState(1);
   return (
     <div className="h-full max-w-5xl py-3 sm:px-0 md:mt-4">
       <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
         <Tab.List className="flex justify-center">
-          {tabs.map((tab, idx) => (
-            <Tab
-              key={idx}
-              as="div"
-              className={({ selected }) =>
-                classNames(
-                  "mx-8 border-b-4 py-2.5 text-xl duration-500 hover:cursor-pointer focus:outline-none focus:outline-0 md:mx-12 ",
-                  selected ? "border-b-amber-500" : "border-b-white"
-                )
-              }
+          <Tab
+            as="div"
+            className={({ selected }) =>
+              classNames(
+                "mx-8 border-b-4 py-2.5 text-xl duration-500 hover:cursor-pointer focus:outline-none focus:outline-0 md:mx-12 ",
+                selected ? "border-b-amber-500" : "border-b-white"
+              )
+            }
+          >
+            <div
+              className={classNames(
+                "mx-1 w-8 duration-300 md:mx-6 md:w-12",
+                selectedIndex == 0
+                  ? "-translate-y-3 scale-125"
+                  : "hover:-translate-y-3 hover:scale-125"
+              )}
             >
-              <div
-                className={classNames(
-                  "mx-1 w-8 duration-300 md:mx-6 md:w-12",
-                  tab.index === selectedIndex
-                    ? "-translate-y-3 scale-125"
-                    : "hover:-translate-y-3 hover:scale-125"
-                )}
-              >
-                <img className="h-full w-full" src={tab.icon} />
-              </div>
-            </Tab>
-          ))}
+              <img className="h-full w-full" src={schedule} />
+            </div>
+          </Tab>
+          <Tab
+            as="div"
+            className={({ selected }) =>
+              classNames(
+                "mx-8 border-b-4 py-2.5 text-xl duration-500 hover:cursor-pointer focus:outline-none focus:outline-0 md:mx-12 ",
+                selected ? "border-b-amber-500" : "border-b-white"
+              )
+            }
+          >
+            <div
+              className={classNames(
+                "mx-1 w-8 duration-300 md:mx-6 md:w-12",
+                selectedIndex == 1
+                  ? "-translate-y-3 scale-125"
+                  : "hover:-translate-y-3 hover:scale-125"
+              )}
+            >
+              <img className="h-full w-full" src={rings} />
+            </div>
+          </Tab>
+          <Tab
+            as="div"
+            className={({ selected }) =>
+              classNames(
+                "mx-8 border-b-4 py-2.5 text-xl duration-500 hover:cursor-pointer focus:outline-none focus:outline-0 md:mx-12 ",
+                selected ? "border-b-amber-500" : "border-b-white"
+              )
+            }
+          >
+            <div
+              className={classNames(
+                "mx-1 w-8 duration-300 md:mx-6 md:w-12",
+                selectedIndex == 2
+                  ? "-translate-y-3 scale-125"
+                  : "hover:-translate-y-3 hover:scale-125"
+              )}
+            >
+              <img className="h-full w-full" src={wedding_location} />
+            </div>
+          </Tab>
         </Tab.List>
         <Tab.Panels className="mt-4 ">
-          {tabs.map((tab, idx) => (
-            <Tab.Panel key={idx} className={classNames("focus:outline-none")}>
-              {tab.posts.map((post, idx) => (
-                <div className="mt-5 w-full flex-col" key={idx}>
-                  {post.texts?.map((text, idx) => (
-                    <div
-                      key={idx}
-                      className="flex w-full justify-center text-xl italic text-white md:text-2xl"
-                    >
-                      <p className="font-serif">{text}</p>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </Tab.Panel>
-          ))}
+          <Tab.Panel className={classNames("focus:outline-none")}></Tab.Panel>
+          <Tab.Panel className={classNames("focus:outline-none")}>
+            {invitation.map((textObj, idx) => (
+              <div className="mt-5 w-full flex-col" key={idx}>
+                {textObj.text.map((line, idx) => (
+                  <div
+                    key={idx}
+                    className="flex w-full justify-center text-xl italic text-white md:text-2xl"
+                  >
+                    <p className="font-serif">{line}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </Tab.Panel>
+          <Tab.Panel className={classNames("focus:outline-none")}></Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
       {/* <div className="h-24 w-24">
