06fd8b0679bf7b86d57e62cda5465ef2381734d4
Ioan Avramov <avramovioan@gmail.com>
2026-01-28 17:07:30 +0200

    feat: adjusted header

--- src/Components/Herader.tsx
+++ src/Components/Herader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="flex items-center justify-between border-b p-3 font-bold">
-      <span className="font-bold">AIWedding</span>
+      <span className="font-bold">Pesho & Lili</span>
     </header>
   );
 }
